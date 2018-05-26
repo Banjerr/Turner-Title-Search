7de4ea3242ed59d40c0fdc9307c7879978ee8f27
Benjamin Redden <benjaminredden@gmail.com>
2018-05-26 14:02:45 -0400

    getting basic api endpoints created and returning results

--- src/server/index.js
+++ src/server/index.js
@@ -1,31 +1,47 @@
-var express = require('express');
-var path = require("path");
-var bodyParser = require('body-parser');
-var MongoClient = require("mongodb").MongoClient;
-var app = express();
-var db;
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const app = express();
+const MongoClient = require('mongodb').MongoClient;
 
-var options = {
+let db;
+
+const MONGO_URI = "mongodb://readonly:turner@ds043348.mongolab.com:43348/dev-challenge";
+
+const options = {
     useNewUrlParser: true
 };
 
-MongoClient.connect("mongodb://readonly:turner@ds043348.mongolab.com:43348/dev-challenge", options, function(err, database) {
-    if (err) throw err;
-
+function initApp(database) {
     db = database;
+    app.listen(3000, () => console.log('running on port 3000!'));    
+}
 
-    app.listen(3000);
-    console.log("Listening on port 3000");
-});
-
-app.get("/", function (req, res) {
-    db.collection("Titles").find({}, function (err, docs) {
-        docs.each(function (err, doc) {
-            if (doc) {
-                console.log(doc);
-            } else {
-                res.end();
-            }
-        });
+app.get('/title', (req, res) => {
+    db.collection('Titles').find({}).toArray((err, docs) => {
+        if (err) throw err;
+        console.log('docs is ', docs);
+        res.send(docs);
     });
 });
+
+app.get('/title/:titleName', (req, res) => {
+  var titleToSearch = req.params.titleName;
+
+  var query = {
+    'TitleName': new RegExp(titleToSearch, 'i')
+  };
+
+  console.log(query);
+
+  db.collection('Titles').find(query).toArray((err, docs) => {
+    if (err) throw err;
+    console.log('docs is ', typeof docs);
+    res.send(docs);
+  });
+});
+
+
+(() => {
+    return MongoClient.connect(MONGO_URI, options).then(client => initApp(client.db()));
+})();
